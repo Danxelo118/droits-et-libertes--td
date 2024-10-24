b47b14fd2d4deab0f69c2edfc5cbfa48fcf5db02
TristanKhadka11 <145160199+TristanKhadka11@users.noreply.github.com>
2024-10-24 15:44:26 -0400

    tristan - installation wordpress

---
+++ page-statique/main.js
@@ -0,0 +1,74 @@
+gsap.to("#logo", {
+    rotation: 360,
+    duration: 5,
+    repeat: -1, // Infinite repeat
+    ease: "linear"
+});
+
+// Activer le plugin ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+// Sélectionner tous les éléments de bullet-item
+const items = document.querySelectorAll('.bullet-item');
+
+// Créer une timeline pour l'animation globale
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".container",
+        start: "-60% top",
+        end: "bottom -50%",
+        scrub: 1,
+        pin: true,
+    }
+});
+
+// Parcourir chaque élément et appliquer l'animation
+items.forEach((item, index) => {
+    tl.fromTo(item, 
+        { opacity: 0, scale: 1.5, y: 20 },
+        { opacity: 1, scale: 1, y: 0, duration: 20, ease: "power2.out" },
+        `+=4`
+    )
+    .to(item, 
+        { opacity: 0, duration: 1, ease: "power2.in" },
+        `+=4`
+    );
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Register ScrollTrigger
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Animate the red title
+    gsap.fromTo(".title-red", 
+        { y: 0 },
+        { 
+            y: -40, 
+            duration: 1,
+            scrollTrigger: {
+                trigger: ".title-red",
+                start: "top 30%",
+                end: "top 80%",
+                scrub: 1,
+        
+            }
+        }
+    );
+
+    // Animate the yellow title
+    gsap.fromTo(".title-yellow", 
+        { y: 0 },
+        { 
+            y: 40, 
+            duration: 1,
+            scrollTrigger: {
+                trigger: ".title-yellow",
+                start: "top 30%",
+                end: "top 80%",
+                scrub: 1,
+             
+            }
+        }
+    );
+});
+
