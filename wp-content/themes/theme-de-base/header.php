0c685cfc968a3bf923f20c495d61c09a820124b9
TristanKhadka11 <145160199+TristanKhadka11@users.noreply.github.com>
2024-11-29 20:25:45 -0500

    modif theme base v2 - tristan

--- wp-content/themes/theme-de-base/header.php
+++ wp-content/themes/theme-de-base/header.php
@@ -8,6 +8,11 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="icon" type="image/x-icon" href="../pages-statiques-tristan/medias/logo.png">
+<title>Ligue des droits et libertés</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
 <title>
 	<?php bloginfo('name'); // Affiche le nom du blog ?> | 
 	<?php is_front_page() ? bloginfo('description') : wp_title(''); // si nous sommes sur la page d'accueil, affichez la description à partir des paramètres du site - sinon, affichez le titre du post ou de la page. ?>
@@ -20,6 +25,7 @@
 /* Cette fonction permet à WordPress et aux extensions d'instancier des fichier CSS et js dans le <head>
 	 Supprimer cette fonction briserait vos extensions et diverses fonctionnalités WordPress. 
 	 Vous pouvez la déplacer si désiré, mais garder là. */
+
 ?>
 </head>
 
@@ -33,21 +39,112 @@
 >
 
 <header>
-	<h1>
-		<a href="<?php echo esc_url( home_url( '/' ) ); // Lien vers la page d'accueil ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); // Title it with the blog name ?>" rel="home"><?php bloginfo( 'name' ); // Affiche le nom du site ?></a>
-	</h1>
 
-	<nav>
-		<?php 
-			// Affiche un menu si dans le tableau de bord un menu a été défini dans cet emplacement
-			wp_nav_menu( array( 'theme_location' => 'main-menu' ) );
-		?>
-	</nav>
+ <div class="promo-banner" id="promo-banner">
+        <p><strong>Travail scolaire - <a href="https://liguedesdroits.ca/" target="_blank">Visitez le vrai site web</a></strong></p>
+        <button id="close-banner">X</button>
+    </div>
 
-	<?php 
-		// Affiche la description de site se trouvant dans "General Settings" dans l'admin WordPress
-		bloginfo( 'description' ); 
-	?>
+    <div class="header">
+    <a class="navbar-brand" href="./index.html">
+        <img src="../pages-statiques-tristan/medias/logo.png" alt="Logo" class="logo" id="logo">
+    </a>
+    <div class="overlay-text1">LUTTER 
+        <br>POUR</div>
+    <div class="overlay-text2">LES DROITS <br>HUMAINS</div>
+    <img src="../pages-statiques-tristan/medias/fond.png" alt="">
+    <video autoplay loop muted>
+        <source src="../pages-statiques-tristan/medias/header.mp4" type="video/mp4">
+        Your browser does not support HTML5 video.
+    </video>
+
+    
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="services-hub.html" id="servicesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Luttes</a>
+                        <div class="dropdown-menu" aria-labelledby="servicesDropdown">
+                            <a class="dropdown-item" href="services-hub.html">Toutes les luttes</a>
+                            <a class="dropdown-item" href="#">COVID-19</a>
+                            <a class="dropdown-item" href="service.html">Droit à la santé</a>
+                            <a class="dropdown-item" href="#">Droit d’association</a>
+                            <a class="dropdown-item" href="#">Droits des personnes en détention</a>
+                            <a class="dropdown-item" href="#">Droits économiques, sociaux et culturels</a>
+                            <a class="dropdown-item" href="#">Droits des personnes migrantes</a>
+                            <a class="dropdown-item" href="#">Droits des peuples autochtones</a>
+                            <a class="dropdown-item" href="#">Environnement et droits humains</a>
+                            <a class="dropdown-item" href="#">Liberté d’expression</a>
+                            <a class="dropdown-item" href="#">Palestine et droits humains</a>
+                            <a class="dropdown-item" href="#">Police et mécanismes de surveillance</a>
+                            <a class="dropdown-item" href="#">Racisme & exclusion sociale</a>
+                            <a class="dropdown-item" href="#">Surveillance des populations</a>
+
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="news-hub.html" id="publicationDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Publications</a>
+                            <div class="dropdown-menu" aria-labelledby="publicationDropdown">
+                                <a class="dropdown-item" href="news-hub.html">Toutes les publications</a>
+                                <a class="dropdown-item" href="#">Revue Droits et libertés</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Nouveau numéro – Regards critiques sur l’incarcération</a>
+                                <a class="dropdown-item" href="#">Dernier numéro – Éducation et droits humains</a>
+                                <a class="dropdown-item" href="#">Tous les numéros de la revue</a>
+                                <a class="dropdown-item" href="#">Éditoriaux et chroniques</a>
+                                <a class="dropdown-item" href="#">S’informer</a>
+                                
+                                <a class="dropdown-item" href="#">Toutes les publications</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Carnets</a>
+                                <a class="dropdown-item" href="#">Communiqués</a>
+                                <a class="dropdown-item" href="#">Déclarations & allocutions</a>
+                                <a class="dropdown-item" href="#">Lettres ouvertes</a>
+                                <a class="dropdown-item" href="#">Mémoires et représentations</a>
+                                <a class="dropdown-item" href="#">Outils d’éducation aux droits</a>
+                                <a class="dropdown-item" href="#">Rapports et analyses</a>
+                                <a class="dropdown-item" href="#">Vidéos</a>
+                             
+    
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="formationsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Formations</a>
+                            <div class="dropdown-menu" aria-labelledby="formationsDropdown">
+                                <a class="dropdown-item" href="#">Ateliers</a>
+                                <a class="dropdown-item" href="#">Demander un atelier</a>
+                                <a class="dropdown-item" href="#">Outils d’éducation aux droits</a>
+                                <a class="dropdown-item" href="#">Lexique</a>
+                         
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="contact-us.html" id="propsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Nous joindre</a>
+                            <div class="dropdown-menu" aria-labelledby="formationsDropdown">
+                                <a class="dropdown-item" href="contact-us.html">Contact</a>
+                        </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link underline" href="#" role="button">Eng</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+    <div class="header-buttons">
+        <button class="btn-donner">
+            <strong>DONNER</strong>
+            <i class="fas fa-wallet button-icon"></i>
+        </button>
+        <button class="btn-informer">
+            <strong>MEMBRE</strong>
+        </button>
+    </div>
+</div>
+
 </header>
 
 <main><!-- Débute le contenu principal de notre site -->
